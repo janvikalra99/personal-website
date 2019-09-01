f502e192a264409c134f161a15144d29adee7eb6
Janvi Kalra <janvi.kalra.21@dartmouth.edu>
2019-09-01 12:33:10 -0700

    completes project layout

--- my-app/src/components/ProjectLayout.js
+++ my-app/src/components/ProjectLayout.js
@@ -1,34 +1,24 @@
 import React from 'react';
-import '../styles/Project.scss';
+import '../styles/ProjectLayout.scss';
 
 class ProjectLayout extends React.Component {
-
-// this should be a map. {challenge: value, skills: value}
-// why is it all sideways?
   render() {
+    // Parse the list of project information passed in.
+    const keys = this.props.synopsis.map((line) =>
+      <div>{line[0]}</div>
+    );
+    const values = this.props.synopsis.map((line) =>
+      <div>{line[1]}</div>
+    );
+
     return (
-      <div class="synopsis">
-        <table id="synposisTable">
-          <tbody>
-            <tr>
-              <td> Challenge </td>
-              <td> {this.props.challenge} </td>
-            </tr>
-            <tr>
-              <td> Skills </td>
-              <td> {this.props.skills} </td>
-            </tr>
-            <tr>
-              <td> Logistics </td>
-              <td> {this.props.logistics} </td>
-            </tr>
-            <tr>
-              <td> Results </td>
-              <td> {this.props.results} </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+        <div id="project-layout">
+          <div id="synopsis">
+            <div class="keys"> {keys} </div>
+            <div class="values"> {values} </div>
+          </div>
+          <div id="title">{this.props.title}</div>
+        </div>
     );
   }
 }
--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -1,5 +1,19 @@
 import React from 'react'
+import ProjectLayout from './ProjectLayout'
 
-const Projects = () => <h1>Projects</h1>
+class Projects extends React.Component {
+
+  render(){
+    const huffman = [
+      ["GOAL", "compressing and decompressing a file"],
+      ["SYNPOSIS", "Individual project | Spring 2018 | Java"],
+      ["KEY SKILLS", "Trees, Maps, Priority Queues | File Input & Output | Implementing Huffman’s Alg*"],
+      ["VALIDATION", "War & Peace novel (587,000 words) 3.2MB -> 1.8MB in 140 nanoseconds"]
+    ]
+
+    return(<ProjectLayout title="huffman encoding" synopsis={huffman}>
+      </ProjectLayout>);
+  }
+}
 
 export default Projects;
